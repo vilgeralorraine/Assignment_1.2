30a5bb34ec577b7df1e81df2a81400fe0603e918
vilgeralorraine <165258193+vilgeralorraine@users.noreply.github.com>
2024-03-29 20:23:19 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
